2f7465333138c446e69a3df5e302e72bb7f779a7
Ferdy Sopian <dev.ferdysopian@gmail.com>
2025-11-04 19:26:29 +0800

    Reformat plugin to follow WPCS standards

--- uninstall.php
+++ uninstall.php
@@ -1,21 +1,21 @@
 <?php
 /**
  * Uninstall script for Admin Bar Pagination
- * 
+ *
  * This file is executed when the plugin is deleted from WordPress admin.
  * It cleans up all plugin data from the database.
- * 
+ *
  * @package AdminBarPagination
  */
 
 // If uninstall not called from WordPress, exit
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit;
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
 }
 
 // Delete plugin options
-if (is_multisite()) {
-    delete_site_option('abp_network_settings');
+if ( is_multisite() ) {
+	delete_site_option( 'abp_network_settings' );
 } else {
-    delete_option('abp_settings');
+	delete_option( 'abp_settings' );
 }
